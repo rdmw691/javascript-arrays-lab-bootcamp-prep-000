853b14b73d144e0341883d0be086a4948e13c528
rdmw691@gmail.com <rdmw691@gmail.com>
2018-07-13 17:49:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Add your functions and code here
-array = ["Milo", "Otis", "Garfield"]
+appendKitten = ["Milo", "Otis", "Garfield"]
 
 function destructivelyAppendKitten(name) {
   array.push(name)
